ba905034ed67cc5d0ffa02f489052a489585cbf0
Evilasivle <evilasivle@gmail.com>
2024-10-17 13:09:07 +0300

    Update Util.java
    ----

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "23e$67A,!";
+    private static final String PASSWORD = "root";
     private static final String databaseName = "users";
 
     // реализуйте настройку соеденения с БД
